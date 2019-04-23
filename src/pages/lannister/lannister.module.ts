6e08c38dab6dc235c247c95b2a2b8cdd8e0f29f0
DANIEL GALLAGHER - STUDENT <G00360986@gmit.ie>
2019-04-23 15:59:17 +0100

    gnfng

---
+++ src/pages/lannister/lannister.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { LannisterPage } from './lannister';
+
+@NgModule({
+  declarations: [
+    LannisterPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(LannisterPage),
+  ],
+})
+export class LannisterPageModule {}
